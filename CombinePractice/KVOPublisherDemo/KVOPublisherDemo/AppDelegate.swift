c198ac0ce28465fbaf3c2430e715b8b5637a54e2
Alexey Sergeev <asergeev@qulix.com>
2021-03-17 16:54:51 +0300

    add: KVOPublisher

---
+++ CombinePractice/KVOPublisherDemo/KVOPublisherDemo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  KVOPublisherDemo
+//
+//  Created by Ben Scheirman on 10/23/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+
+
+}
+
