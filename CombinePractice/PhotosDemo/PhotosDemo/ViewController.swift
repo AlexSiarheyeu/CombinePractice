75256a25033e1b3e79ee3b3e83f9b5cfc25e1110
Alexey Sergeev <asergeev@qulix.com>
2021-03-24 16:28:46 +0300

    lesson finished

--- CombinePractice/PhotosDemo/PhotosDemo/ViewController.swift
+++ CombinePractice/PhotosDemo/PhotosDemo/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     
     private var cancellables: Set<AnyCancellable> = []
     private var datasource: UICollectionViewDiffableDataSource<Int, UIImage>!
+    private var loadPhotoCancellable: AnyCancellable?
+    private var loadImagesCancellable: AnyCancellable?
     
     @IBOutlet var collectionView: UICollectionView!
     
@@ -35,11 +37,50 @@ class ViewController: UIViewController {
         
         let targetSize = CGSize(width: 512, height: 512)
         
+        let assetSubject = PassthroughSubject<PHAsset, Never>()
+        
+        loadImagesCancellable = assetSubject
+            .flatMap { self.imagePublisher(for: $0, targetSize: targetSize, contentMode: .aspectFill)}
+            .compactMap {$0}
+            .collect()
+            .sink { images in
+                var snapshot = NSDiffableDataSourceSnapshot<Int, UIImage>()
+                snapshot.appendSections([0])
+                snapshot.appendItems(images)
+                self.datasource.apply(snapshot, animatingDifferences: true, completion: nil)
+            }
+            
+        
+        fetchResult
+            .enumerateObjects {asset, index, _ in
+                assetSubject.send(asset)
+            }
+        
+        assetSubject
+            .send(completion: .finished)
         
     }
     
+    private var authorizationStatusPublisher:
+        AnyPublisher<PHAuthorizationStatus, Never> = {
+            
+            Deferred {
+                Future { promise in
+                    PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+                        promise(.success(status))
+                    }
+                }
+            }
+            .eraseToAnyPublisher()
+    }()
+    
     @IBAction func loadPhotosTapped(_ sender: Any) {
-        
+            loadPhotoCancellable = authorizationStatusPublisher
+                .drop(while: { $0 != .authorized })
+                .sink { [weak self] _ in
+                    self?.fetchPhotos()
+                }
+                
     }
     
     private func imagePublisher(for asset: PHAsset, targetSize: CGSize, contentMode: PHImageContentMode) -> AnyPublisher<UIImage?, Never> {
@@ -48,7 +89,12 @@ class ViewController: UIViewController {
         requestOptions.deliveryMode = .fastFormat
         requestOptions.resizeMode = .exact
         
-        fatalError()
+        return Future { promise in
+            PHImageManager.default().requestImage(for: asset, targetSize: targetSize, contentMode: contentMode, options: requestOptions) { (image, info) in
+                promise(.success(image))
+            }
+        }
+        .eraseToAnyPublisher()
     }
 }
 
